46cbba84708e343234158c06df1214792b482077
Justin Riley <20hz20khz@gmail.com>
2024-09-18 15:16:11 -0400

    Update BaseballPlayer.js

--- BaseballPlayer.js
+++ BaseballPlayer.js
@@ -11,17 +11,47 @@ class BaseballPlayer {
         }
     }
 
+    static normalizeToTen(data) {
+        data = Math.abs(data) % 20;
+        if(data <= 10){
+            return data;
+        }
+        return data - (data - 10);
+    }
+
     // Constructor to initialize player attributes
     constructor(name, team, position) {
-        this.name = name; // Player's name
+        this.firstName = name; // Player's name
+        this.lastName = name;
+        this.fullname = this.firstName + this.lastName;
         this.team = team; // Team name
         this.position = position; // Position on the field
         this.stats = new Stats();
     }
 
+    
+
     // Method to display player information
     displayInfo() {
         return `Name: ${this.name}\nTeam: ${this.team}\nPosition: ${this.position}\nBatting Average: ${this.battingAverage.toFixed(3)}\nHome Runs: ${this.homeRuns}`;
+    }
+
+    isSwingingBat(pitchNumber, pitchScore){
+        pitchScore = BaseballPlayer.normalizeToTen(pitchScore);
+        let pickiness = BaseballPlayer.normalizeToTen(this.firstName.charCodeAt(pitchNumber % this.firstName.length) + pitchNumber);
+        if(pitchScore >= pickiness * 0.75){
+            return true;
+        }
+        return false;
+    }
+
+    isHittingBall(pitchNumber, pitchScore){
+        pitchScore = BaseballPlayer.normalizeToTen(pitchScore);
+        let swingAccuracy = BaseballPlayer.normalizeToTen(this.lastName.charCodeAt(pitchNumber % this.lastName.length) + pitchNumber);
+        if(swingAccuracy >= pitchScore){
+            return true;
+        }
+        return false;
     }
 
 
