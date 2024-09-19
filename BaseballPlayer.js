01acc2abb37d16e79f01f2bb29c672fbd2fa2d45
Justin Riley <20hz20khz@gmail.com>
2024-09-19 11:27:53 -0400

    Update BaseballPlayer.js

--- BaseballPlayer.js
+++ BaseballPlayer.js
@@ -11,22 +11,29 @@ class BaseballPlayer {
         }
     }
 
+    // Keeps values in the range (0...10) inclusive
     static normalizeToTen(data) {
         data = Math.abs(data) % 20;
         if(data <= 10){
             return data;
         }
-        return data - (data - 10);
+        return 10 - (data - 10);
     }
 
     // Constructor to initialize player attributes
-    constructor(name, team, position) {
+    constructor(name) {
         this.firstName = name; // Player's name
         this.lastName = name;
         this.fullname = this.firstName + this.lastName;
-        this.team = team; // Team name
-        this.position = position; // Position on the field
+        this.team = "null"; // Team name
+        this.position = "null"; // Position on the field
         this.stats = new Stats();
+        this.pitchScoreAverage = BaseballPlayer.normalizeToTen(Math.random()*6 + Math.random()*6);
+        this.pitchScoreDeviation = 1 + Math.random() + Math.random();
+        this.swingPercent = BaseballPlayer.normalizeToTen(Math.random()*6 + Math.random()*6) * 10;
+        this.contactPercent = BaseballPlayer.normalizeToTen(Math.random()*6 + Math.random()*6) * 10;
+        this.hitScoreAverage = BaseballPlayer.normalizeToTen(Math.random()*6 + Math.random()*6);
+        this.hitScoreDeviation = 1 + Math.random() + Math.random();
     }
 
     
@@ -36,22 +43,39 @@ class BaseballPlayer {
         return `Name: ${this.name}\nTeam: ${this.team}\nPosition: ${this.position}\nBatting Average: ${this.battingAverage.toFixed(3)}\nHome Runs: ${this.homeRuns}`;
     }
 
+    // Pitching methods
+
+    getPitchScore(pitchNumber){
+        let tiredness = pitchNumber * 0.01;
+        return BaseballPlayer.normalizeToTen(this.pitchScoreAverage + Math.sin(pitchNumber) * this.pitchScoreDeviation - tiredness);
+    }
+
+    // Batting methods
+
     isSwingingBat(pitchNumber, pitchScore){
+        let tiredness = pitchNumber * 0.01;
         pitchScore = BaseballPlayer.normalizeToTen(pitchScore);
-        let pickiness = BaseballPlayer.normalizeToTen(this.firstName.charCodeAt(pitchNumber % this.firstName.length) + pitchNumber);
-        if(pitchScore >= pickiness * 0.75){
+        // player's prefer to swing at better pitches aka higher pitch scores
+        if(pitchScore >= (100 - this.swingPercent)/10 + tiredness || this.swingPercent > Math.random()*100 + tiredness){
             return true;
         }
         return false;
     }
 
-    isHittingBall(pitchNumber, pitchScore){
+    isContactingBall(pitchNumber, pitchScore){
+        let tiredness = pitchNumber * 0.01;
         pitchScore = BaseballPlayer.normalizeToTen(pitchScore);
-        let swingAccuracy = BaseballPlayer.normalizeToTen(this.lastName.charCodeAt(pitchNumber % this.lastName.length) + pitchNumber);
-        if(swingAccuracy >= pitchScore){
+        // the lower the pitch score, the easier to hit
+        if(pitchScore <= this.contactPercent/10 - tiredness ||  this.contactPercent > Math.random()*100 + tiredness){
             return true;
         }
         return false;
+    }
+
+    getHitScore(pitchNumber, pitchScore){
+        let tiredness = pitchNumber * 0.01;
+        pitchScore = BaseballPlayer.normalizeToTen(pitchScore);
+        return BaseballPlayer.normalizeToTen(this.hitScoreAverage * pitchScore * 0.1 + Math.sin(pitchNumber) * this.hitScoreDeviation - tiredness);
     }
 
 
