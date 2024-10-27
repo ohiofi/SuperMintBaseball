742c3d8c719bf813ff1d68535a1b35dd293415d8
Justin Riley <20hz20khz@gmail.com>
2024-10-27 13:58:39 -0400

    show game messages on webpage

--- BaseballGame.js
+++ BaseballGame.js
@@ -6,12 +6,13 @@ class BaseballGame {
         console.log("yo")
         this.homeTeam = new BaseballTeam();
         this.awayTeam = new BaseballTeam();
+        this.name = this.awayTeam.getName() + ": 0 " + this.homeTeam.getName() + ": 0";
         this.pitchNumber = 0;
         this.defenseTeam = this.homeTeam;
         this.offenseTeam = this.awayTeam;
         this.batter = null;
         this.pitcher = null;
-        console.log(this.awayTeam.getName() + " @ " + this.homeTeam.getName())
+        
         console.log(Name.generateAd())
         this.nextBatter();
         this.count = {
@@ -22,7 +23,9 @@ class BaseballGame {
 
     }
 
-    
+    getName(){
+        return this.name;
+    }
 
     nextBatter() {
         this.batter = this.offenseTeam.getNextBatter();
@@ -31,7 +34,7 @@ class BaseballGame {
 
     nextPitch() {
         this.pitchNumber++;
-        this.atBat();
+        return this.atBat();
     }
 
     resetTheCount(){
@@ -44,35 +47,41 @@ class BaseballGame {
 
 
     atBat() {
+        let result = "";
         this.pitcher = this.defenseTeam.getPitcher();
         let pitchScore = this.pitcher.getPitchScore(this.pitchNumber);
         let pitchDescription = BaseballGame.pitchDescriptions[Math.floor(pitchScore*BaseballGame.pitchDescriptions.length/10)];
         
         
-        console.log(this.pitcher.getName() + " throws a " + pitchDescription + " pitch");
-        console.log("[pitchScore = " + pitchScore + "]")
+        //console.log(this.pitcher.getName() + " throws a " + pitchDescription + " pitch");
+        result += this.pitcher.getName() + " throws a " + pitchDescription + " pitch"
+        //console.log("[pitchScore = " + pitchScore + "]")
         if (this.batter.isSwingingBat(this.pitchNumber, pitchScore)) {
             // batter swings
-            console.log(this.batter.getName() + " swings")
+            //console.log(this.batter.getName() + " swings")
+            result += "<br>" + this.batter.getName() + " swings";
             if (this.batter.isContactingBall(this.pitchNumber, pitchScore)) {
                 // makes contact
                 let hitScore = this.batter.getHitScore(this.pitchNumber, pitchScore);
-                console.log("[hitScore = " + hitScore + "]")
+                //console.log("[hitScore = " + hitScore + "]")
                 // foul, out, hit a single, double, triple, home run
                 // 50% of weak hits are foul
                 if (hitScore <= 2.5 && rng.random() > 0.5) {
-                    console.log(this.batter.getName() + " hits a foul ball");
+                    //console.log(this.batter.getName() + " hits a foul ball");
+                    result += "<br>" + this.batter.getName() + " hits a foul ball"
                     if(this.count.strikes < 2){
                         this.count.strikes++;
                     }
                 } else {
-                    console.log(this.batter.getName() + " hits a fly ball")
+                    //console.log(this.batter.getName() + " hits a fly ball")
+                    result += "<br>" + this.batter.getName() + " hits a fly ball"
                     let defender = this.defenseTeam.getRandomPlayer();
-                    console.log("[defender = " + defender.getName() + "]")
+                    //console.log("[defender = " + defender.getName() + "]")
                     let defenseScore = defender.getDefenseScore(this.pitchNumber);
-                    console.log("[defenseScore = " + defenseScore + "]")
+                    //console.log("[defenseScore = " + defenseScore + "]")
                     if (defenseScore >= hitScore) {
-                        console.log(defender.getName() + " makes the catch. OUT!")
+                        //console.log(defender.getName() + " makes the catch. OUT!")
+                        result += "<br>" + defender.getName() + " makes the catch. OUT!"
                         defender.setHungerDown()
                         this.batter.setHungerUp()
                         this.count.outs++;
@@ -83,45 +92,52 @@ class BaseballGame {
                         }
                         
                     } else if (hitScore / defenseScore <= 1.5) {
-                        console.log(this.batter.getName() + " hits a SINGLE")
+                        //console.log(this.batter.getName() + " hits a SINGLE")
+                        result += "<br>" + this.batter.getName() + " hits a SINGLE"
                     } else if (hitScore / defenseScore <= 2.5) {
                         console.log(this.batter.getName() + " hits a DOUBLE")
+                        result += "<br>" + this.batter.getName() + " hits a DOUBLE"
                     } else if (hitScore / defenseScore <= 3.5) {
                         console.log(this.batter.getName() + " hits a TRIPLE")
+                        result += "<br>" + this.batter.getName() + " hits a TRIPLE"
                     } else {
                         console.log(this.batter.getName() + " hits a HOME RUN!")
+                        result += "<br>" + this.batter.getName() + " hits a HOME RUN!"
                     }
                 }
-
             } else {
                 // swinging but doesn't make contact
-                console.log("STRIKE Swinging");
+                //console.log("STRIKE Swinging");
+                result += "<br>" + "STRIKE Swinging"
                 this.count.strikes++;
             }
         } else {
-            console.log(this.batter.getName() + " is looking")
+            //console.log(this.batter.getName() + " is looking")
+            result += "<br>" + this.batter.getName() + " is looking"
             // batter doesn't swing
             if (pitchScore > 5) {
-                console.log("STRIKE Looking");
+                //console.log("STRIKE Looking");
+                result += "<br>" + "STRIKE Looking"
                 this.count.strikes++;
             } else {
-                console.log("BALL Looking")
+                //console.log("BALL Looking")
+                result += "<br>" + "BALL Looking"
                 this.count.balls++;
-                if(this.count.balls >= 4){
-                    this.batter.setHungerDown()
-                    this.pitcher.setHungerUp()
-                    this.count.balls = 0;
-                    this.count.strikes = 0;
-                    console.log(this.batter.getName() + " walks")
-                    this.nextBatter();
-                }
             }
         }
-        console.log("Balls: " + this.count.balls +
-         " / Strikes: " + this.count.strikes +
-         " / Outs: " + this.count.outs + "");
+        //console.log("Balls: " + this.count.balls +
+        // " / Strikes: " + this.count.strikes +
+        // " / Outs: " + this.count.outs + "");
+        result += "<br>"
+        result += "<sub class='baseIndicator leftBaseIndicator'>◇</sub>"
+        result += "<sup class='baseIndicator centerBaseIndicator'>◇</sup>"
+        result += "<sub class='baseIndicator rightBaseIndicator'>◇</sub>"
+        result += "" + "B: " + this.count.balls +
+        " / S: " + this.count.strikes +
+        " / O: " + this.count.outs
         if(this.count.strikes >= 3){
-            console.log(this.batter.getName() + " STRIKES OUT");
+            //console.log(this.batter.getName() + " STRIKES OUT");
+            result += "<br>" + this.batter.getName() + " STRIKES OUT"
             this.batter.setHungerUp()
             this.pitcher.setHungerDown()
             this.count.outs++;
@@ -131,16 +147,28 @@ class BaseballGame {
                 this.nextBatter();
             }
         }
+        if(this.count.balls >= 4){
+            this.batter.setHungerDown()
+            this.pitcher.setHungerUp()
+            this.count.balls = 0;
+            this.count.strikes = 0;
+            //console.log(this.batter.getName() + " walks")
+            result += "<br>" + this.batter.getName() + " walks"
+            this.nextBatter();
+        }
         if(this.count.outs >= 3){
             let temp = this.defenseTeam
             this.defenseTeam = this.offenseTeam
             this.offenseTeam = temp
             this.resetTheCount();
             
-            console.log("THREE OUTS");
-            console.log(this.offenseTeam.getName() + " turn to bat")
+            //console.log("THREE OUTS");
+            result += "<br>" + "THREE OUTS"
+            //console.log(this.offenseTeam.getName() + " turn to bat")
+            result += "<br>" + this.offenseTeam.getName() + "'s turn to bat"
             this.nextBatter();
         }
+        return result;
     }
 
 
--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 rng = new MersenneTwister(parseInt(2024));
 
 let timeIntervalId;
+let bgame = new BaseballGame();
 
-function showBlockMessage(message){
+function showBlockMessage(message, gameName){
   const container = document.getElementById('messageContainer');
 
     // Create a new div for the message
@@ -11,16 +12,17 @@ function showBlockMessage(message){
     postDiv.className = 'post';
 
     // Add content to the post
-    const username = 'User123'; // Example username
+    //const username = 'User123'; // Example username
     const timestamp = new Date().toLocaleTimeString(); // Example timestamp
 
     postDiv.innerHTML = `
-        <span class="username">${username}</span> <span class="timestamp">${timestamp}</span>
+        <span class="username">${gameName}</span> <span class="timestamp">${timestamp}</span>
         <p>${message}</p>
     `;
   
   // Check if the user is scrolled to the bottom
     const isScrolledToBottom = container.scrollHeight - container.clientHeight <= container.scrollTop + 1;
+    const previousScrollTop = container.scrollTop;
 
     // Append the new post to the container
     container.appendChild(postDiv);
@@ -30,10 +32,12 @@ function showBlockMessage(message){
     // Scroll to the bottom only if the user is already at the bottom
     if (isScrolledToBottom) {
         container.scrollTop = container.scrollHeight;
+    } else {
+        container.scrollTop = previousScrollTop;
     }
 }
 
-function startMessageInterval(speed) {
+function startMessageInterval(speed,bgame) {
     
     // Clear any existing interval
     if (timeIntervalId) {
@@ -42,20 +46,42 @@ function startMessageInterval(speed) {
 
     // Set a new interval
     timeIntervalId = setInterval(() => {
-        showBlockMessage("This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!".substring(parseInt(Math.random()*("This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!".length))));
+        showBlockMessage(bgame.nextPitch(),bgame.getName());
+        //showBlockMessage("This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!".substring(parseInt(Math.random()*("This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!This is a new message!".length))));
     }, speed);
 }
 
 // Set initial interval
-startMessageInterval(2000);
+startMessageInterval(2000, bgame);
 
 // Change interval based on user input
 document.getElementById('speedSlider').onchange = function() {
     const newInterval = 5200 - parseInt(document.getElementById('speedSlider').value, 10);
   
     if (!isNaN(newInterval) && newInterval > 0) {
-        startMessageInterval(newInterval);
+        startMessageInterval(newInterval,bgame);
     } else {
         alert("Please enter a valid positive number.");
     }
 };
+
+// News Ticker
+(function() {
+    // var t0 = performance.now();
+    setInterval(function() {
+      var parent = document.querySelector('.newsTickerContainer');
+      var slide = parent.querySelectorAll('.newsTickerItem');
+  
+      for (var i = 0; i < slide.length; i++) {
+        var x = slide[i];
+        x.classList.toggle('sliding-now');
+      }
+  
+      setTimeout(function() {
+        parent.appendChild(slide[0]);
+      }, 5000);
+  
+    }, 5000);
+    // var t1 = performance.now();
+    // console.log("Carousel taking " + (t1 - t0) + " milliseconds.");
+  })()
