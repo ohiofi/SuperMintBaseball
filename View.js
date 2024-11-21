426544ed746521e5b32e22f19f78e8dc4a66c7ef
Justin Riley <20hz20khz@gmail.com>
2024-11-21 16:42:59 -0500

    Revert "fix ticker"
    This reverts commit de1f35fbe0732980543ede2ab57122ebab87aaf7.

--- View.js
+++ View.js
@@ -5,10 +5,10 @@ class View {
       element.id = idName;
     }
     if (classNames && classNames.constructor === Array) {
-      for (let each of classNames) {
+      for(let each of classNames){
         element.classList.add(each);
       }
-    } else if (classNames) {
+    } else if(classNames){
       element.classList.add(classNames);
     }
     if (content) {
@@ -19,73 +19,92 @@ class View {
 
   constructor() {
     this.app = this.getElement("#root");
-
+    
     this.newsTickerContainer = View.createElement("div", "newsTickerContainer", ["mt-4"]);
     this.tickerItems = [];
     this.pageMenuBar = View.createElement("ul", null, "pagination");
     this.addMenuBarItemHome()
     this.addMenuBarItemLive()
-
+    
     this.pageContainer = View.createElement("div", "pageContainer");
-
+    
     this.homePage = new HomePage();
     this.pageContainer.append(this.homePage.render());
-
+    
     this.liveGamesPage = new LiveGamesPage();
     this.pageContainer.append(this.liveGamesPage.render());
 
     this.standingsPage;
 
     this.singleGamePages = [];
-
+    
     this.gameWidgetContainer = View.createElement("div", "game-widget-container", "container");
     this.gameWidgetItems = [];
     this.modal = new StatsModal();
-    this.app.append(this.newsTickerContainer, this.pageMenuBar, this.pageContainer, this.modal.render())
+    this.app.append(this.newsTickerContainer, this.pageMenuBar,this.pageContainer,this.modal.render())
+    
+    
   }
 
   // adds the individual "posts" that show up in the feed on each Game Page
-  addGameMessages(gameMessages) {
-    for (let i = 0; i < gameMessages.length; i++) {
-      if (gameMessages[i].done) {
-        continue;
-      }
-      const container = this.singleGamePages[i].messageFeedContainer;
-      const postDiv = new GamePost(gameMessages[i])
-      // Append the new post to the container while maintaining scroll
-      const previousScrollTop = container.scrollTop;
-      if (container.scrollHeight - container.clientHeight <= container.scrollTop) {
-        container.appendChild(postDiv.render());
-        container.scrollTop = container.scrollHeight;
-        this.singleGamePages[i].messageJumpButton.classList.add("hide");
-      } else {
-        container.appendChild(postDiv.render());
-        container.scrollTop = previousScrollTop;
-        this.singleGamePages[i].messageJumpButton.classList.remove("hide");
-      }
-    }
+  addGameMessages(gameMessages){
+      for(let i=0;i<gameMessages.length;i++){
+        if(gameMessages[i].done){
+          continue;
+        }
+        
+        const container = this.singleGamePages[i].messageFeedContainer;
+        // // Create a new div for the message
+        // const postDiv = View.createElement('div',null,['post',"bg-111"]);
 
+        // const timestamp = new Date().toLocaleTimeString(); 
+    
+        // postDiv.innerHTML = `
+        //     <span class="username">${gameMessages[i].scoreString}</span> <span class="timestamp">${timestamp}</span>
+        //     <p>${gameMessages[i].log}</p>
+        //     <p class="font-monospace">${gameMessages[i].baseIcons} B: ${gameMessages[i].count.balls} S: ${gameMessages[i].count.strikes} O: ${gameMessages[i].count.outs}</p>
+        // `;
+        const postDiv = new GamePost(gameMessages[i])
+
+        // Append the new post to the container while maintaining scroll
+        const previousScrollTop = container.scrollTop;
+        if(container.scrollHeight - container.clientHeight <= container.scrollTop){
+          container.appendChild(postDiv.render());
+          container.scrollTop = container.scrollHeight;
+          this.singleGamePages[i].messageJumpButton.classList.add("hide");
+        }else{
+          container.appendChild(postDiv.render());
+          container.scrollTop = previousScrollTop;
+          this.singleGamePages[i].messageJumpButton.classList.remove("hide");
+        }
+    }
+    
   }
 
-  addAllSingleGamePages(game) {
+  addAllSingleGamePages(game){
     // this.addPageHome(game);
     const scores = game.getScores();
     for (let i = 0; i < scores.length; i++) {
-      this.singleGamePages[i] = new SingleGamePage(i, scores[i]);
+      this.singleGamePages[i] = new SingleGamePage(i,scores[i]);
       this.pageContainer.append(this.singleGamePages[i].render());
     }
   }
 
+  
+ 
+  
+
+  
 
   addMenuBarItemSingleGamePages(game) {
-
+    
     //<li class="page-item"><a class="page-link" href="#">1</a></li>
-
+    
     const scores = game.getScores();
     for (let i = 0; i < scores.length; i++) {
       const menuItem = View.createElement("li", null, "page-item");
-
-      const menuLink = View.createElement("a", null, ["page-link", "bg-dark", "border-0", "link-offset-2", "link-light", "link-underline-opacity-10", "link-underline-opacity-100-hover"]);
+      
+      const menuLink = View.createElement("a", null, ["page-link","bg-dark","border-0","link-offset-2","link-light","link-underline-opacity-10","link-underline-opacity-100-hover"]);
       // menuLink.classList.add("bg-dark")
       // menuLink.classList.add("border-0")
       menuLink.textContent = i;
@@ -100,13 +119,13 @@ class View {
       });
       menuItem.append(menuLink);
       this.pageMenuBar.append(menuItem);
-
+      
     }
   }
 
   addMenuBarItemHome() {
     const menuItem = View.createElement("li", null, "page-item");
-    const menuLink = View.createElement("a", null, ["page-link", "bg-dark", "border-0"])
+    const menuLink = View.createElement("a", null, ["page-link","bg-dark","border-0"])
     menuLink.textContent = "🏠";
     menuLink.addEventListener('click', event => {
       const els = document.getElementsByClassName("page");
@@ -114,7 +133,7 @@ class View {
         el.classList.add("hide")
       });
       document.getElementById("homePage").classList.remove("hide");
-
+      
     })
     menuItem.append(menuLink);
     this.pageMenuBar.append(menuItem);
@@ -125,22 +144,33 @@ class View {
     const newsTickerRibbon = View.createElement("p", "newsTickerRibbon", null);
     const scores = game.getScores();
     // add TWICE as many items as there are games
-    let multiplier = 2;
-    if (scores.length < 3) multiplier = 4
-    for (let i = 0; i < scores.length * multiplier; i++) {
+    for(let i=0;i<scores.length * 2;i++){
       this.tickerItems[i] = View.createElement("span", null, ["newsTickerItem"]);
       this.tickerItems[i].innerHTML = scores[i % scores.length];
-
+      
       newsTickerRibbon.append(this.tickerItems[i]);
     }
     this.newsTickerContainer.append(newsTickerRibbon);
   }
 
+  addPageGamePage(score,gameNum){
+    
+  }
+
+ 
+
+  addPageLiveGames(game){
+
+  }
+
+  
+
+  
 
 
   addMenuBarItemLive() {
     const menuItem = View.createElement("li", null, "page-item");
-    const menuLink = View.createElement("a", null, ["page-link", "bg-dark", "border-0"])
+    const menuLink = View.createElement("a", null, ["page-link","bg-dark","border-0"])
     menuLink.textContent = "🏟️";
     menuLink.addEventListener('click', event => {
       const els = document.getElementsByClassName("page");
@@ -148,13 +178,31 @@ class View {
         el.classList.add("hide")
       });
       document.getElementById("liveGamesPage").classList.remove("hide");
-
+      
     })
     menuItem.append(menuLink);
     this.pageMenuBar.append(menuItem);
   }
 
+  // bindPageMenuBar(handler) {
+  //   this.pageMenuBar.addEventListener('click', event => {
+  //     if (event.target.className === 'delete') {
+  //       const id = parseInt(event.target.parentElement.id)
 
+  //       handler(id)
+  //     }
+  //   })
+  // }
+
+
+  
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector)
+
+    return element
+  }
 
   showPage(pageName) {
     const els = document.getElementsByClassName("page");
@@ -162,8 +210,11 @@ class View {
       el.classList.add("hide")
     });
     document.getElementById(pageName).classList.remove("hide");
-
+    const container = document.getElementById(pageName).children[1]
+    container.scrollTop = container.scrollHeight
+    // Check if the user is scrolled to the bottom
+    // document.getElementById(pageName).children[1].scrollTo(0,document.getElementById(pageName).children[1].scrollHeight)
   }
 
-
+  
 }
