e18d9f47d74454fe154b3027511d1cad5f466afe
Justin Riley <20hz20khz@gmail.com>
2024-12-10 15:34:27 -0500

    Update crestMaker.py

--- utilities/svgPathHelpers/crestMaker.py
+++ utilities/svgPathHelpers/crestMaker.py
@@ -2,6 +2,9 @@
 from turtle import *
 svgPath = ""
 
+def getPoint():
+    return ""+xcor()+","+ycor()+" "
+
 def drawBorders():
     pd()
     goto(0,100)
@@ -9,4 +12,30 @@ def drawBorders():
     goto(100,0)
     home()
 
+
+def mlsCrest():
+    pu()
+    goto(50,2)
+    pd()
+    seth(25)
+    for i in range(80):
+        fd(1)
+        lt(.7)
+    mysteryHeading = heading()
+    lt(90-mysteryHeading)
+    fd(33)
+    lt(90)
+    x=xcor()
+    goto(100-x, ycor())
+    lt(90)
+    fd(33)
+    lt(90-mysteryHeading)
+    for i in range(80):
+        fd(1)
+        lt(.7)
+    pu()
+    home()
+
 drawBorders()
+mlsCrest()
+update()
